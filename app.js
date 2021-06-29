13348e28626b7257a2ffc8f833f0d826f155aa5d
7204-OMER <Eyupbhr24@gmail.com>
2021-06-29 22:08:55 +0300

    add app.js

--- app.js
+++ app.js
@@ -1,27 +1,24 @@
-let btn = document.querySelector("#btn")
+let btn = document.querySelector("#btn");
 let anwer = document.getElementById("answer");
 let counter = 0;
 let count = document.getElementById("count");
-let wanted = parseInt(100*Math.random());
+let wanted = parseInt(100 * Math.random());
 let input = document.getElementById("num");
-num.addEventListener("keyup", function(event){
-    if(event.code == "Enter" || event.code == "NumpadEnter"){
-        checkedNumber();
-    }
-})
-btn.addEventListener("click",checkedNumber);
-function checkedNumber(){
-    let num  = document.getElementById("num").value;
-    counter++;
-    count.innerHTML = "Number of attemps: " + counter;
-    console.log(wanted);
-    console.log(answer);
-    console.log(num)
-        if (parseInt(num) === wanted){
-            answer.textContent = "Congrats!!";
-        }else if (parseInt(num) < wanted){
-            answer.textContent = "Try Higher Numbers!!";
-        }else if (parseInt(num) > wanted){
-            answer.textContent = "Try Lower Numbers!!";
-        }    // location.reload();
+num.addEventListener("keyup", function (event) {
+  if (event.code == "Enter" || event.code == "NumpadEnter") {
+    checkedNumber();
+  }
+});
+btn.addEventListener("click", checkedNumber);
+function checkedNumber() {
+  let num = document.getElementById("num").value;
+  counter++;
+  count.innerHTML = "Number of attemps: " + counter;
+  if (parseInt(num) === wanted) {
+    answer.textContent = "Congrats!!";
+  } else if (parseInt(num) < wanted) {
+    answer.textContent = "Try Higher Numbers!!";
+  } else if (parseInt(num) > wanted) {
+    answer.textContent = "Try Lower Numbers!!";
+  } // location.reload();
 }
